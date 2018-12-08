c78e293a0efcd844d6eb315f261155541e4d6b9a
gosai09 <gosai09@gmail.com>
2018-12-08 08:12:41 +0530

    a
    a

---
+++ Controllers/UserManageController.cs
@@ -0,0 +1,54 @@
+﻿using PartialViewExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialViewExample.Controllers
+{
+    public class UserManageController : Controller
+    {
+        // GET: UserManage
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserModel userModel)
+        {
+            try
+            {
+                //for show error
+               // int a = int.Parse(userModel.UserPassword);
+                if (userModel.UserName == "abc" && userModel.UserPassword == "123")
+                {
+                    
+                    Session["userId"] = 1;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["error"] = "User Name or Password is Wrong!";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                TempData["error"] = "Server Side error: " + ex.Message;
+                return RedirectToAction("ErrorShow");
+            }
+            return View();
+        }
+        public ActionResult ErrorShow()
+        {
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            TempData["message"] = "Successfully LogOut!";
+            return RedirectToAction("Login","UserManage");
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartialViewExample.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string UserPassword { get; set; }
+
+    }
+}
